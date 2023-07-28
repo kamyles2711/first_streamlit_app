7ad4d859711ae5072cc4d4dc05ee0d0e1b652754
kamyles2711 <68362289+kamyles2711@users.noreply.github.com>
2023-07-28 10:40:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as sl
 
 sl.title('My Parents New Healthy Diner')
 
-sl.markdown("Omega 3 & Blueberry Oatmeal")
+sl.markdown('Omega 3 & Blueberry Oatmeal')
 sl.markdown('Kale, Spinach & Rocket Smoothie')
 sl.markdown('Hard-Boiled Free-Range Egg')
