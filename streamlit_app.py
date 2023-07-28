51684802b5c5ca3f0974b4d382ba3374ba8a21ff
kamyles2711 <68362289+kamyles2711@users.noreply.github.com>
2023-07-28 10:34:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.print('Omega 3 & Blueberry Oatmeal')
+streamlit.print('Kale, Spinach & Rocket Smoothie')
+streamlit.print('Hard-Boiled Free-Range Egg')
