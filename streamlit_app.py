2245342146a54bdf2b4428e7104db91b6bf38ab5
kamyles2711 <68362289+kamyles2711@users.noreply.github.com>
2023-07-28 10:44:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ sl.title('My Parents New Healthy Diner')
 
 sl.header('Breakfast Menu')
 
-sl.text('Omega 3 & Blueberry Oatmeal')
-sl.text('Kale, Spinach & Rocket Smoothie')
-sl.text('Hard-Boiled Free-Range Egg')
+sl.text('🥣 Omega 3 & Blueberry Oatmeal')
+sl.text('🥗 Kale, Spinach & Rocket Smoothie')
+sl.text('🐔 Hard-Boiled Free-Range Egg')
+sl.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
