07d918ef4ab4486ec3fbf788d6a4a43fea8618a8
kamyles2711 <68362289+kamyles2711@users.noreply.github.com>
2023-07-31 10:56:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,3 +28,8 @@ sl.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 sl.text(fruityvice_response.json())
 
+# Normalize Data
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Put Data into DataFrame
+streamlit.dataframe(fruityvice_normalized)
+
